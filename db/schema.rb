41142aa88e435953ee29f86ee55b40fa7944f64b
MiBBy-4 <maksim.762373maks@bk.ru>
2022-01-30 13:21:09 +0300

    feat: create body feild in database that used for describe your aim of todo task, also add it like parameter in TodosController in todo_params method

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_095123) do
+ActiveRecord::Schema.define(version: 2022_01_30_095758) do
 
   create_table "todos", force: :cascade do |t|
     t.text "task"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
   end
 
 end
